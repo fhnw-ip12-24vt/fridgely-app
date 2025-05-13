db74c5414774299002c635295cf0751a25d2aa63
Diego Varela <diego.varelapenas@students.fhnw.ch>
2025-05-13 19:39:26 +0200

    Refactor UIConfig to improve organization and logging; adjust setUIFont methods for better resource handling

--- src/main/java/ch/primeo/fridgely/config/UIConfig.java
+++ src/main/java/ch/primeo/fridgely/config/UIConfig.java
@@ -1,12 +1,14 @@
 package ch.primeo.fridgely.config;
 
-import javax.swing.*;
-import java.awt.*;
-import java.io.File;
-import java.io.InputStream;
-
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
+
+import javax.swing.UIManager;
+import java.awt.Color;
+import java.awt.Font;
+import java.io.InputStream;
+import java.util.Enumeration;
+import java.util.logging.Logger;
 
 public class UIConfig {
 
@@ -19,13 +21,14 @@ public class UIConfig {
     public static final Color ACTIVE_COLOR = new Color(131, 180, 225);
     public static final Color DISABLED_COLOR = new Color(193, 193, 193);
 
+    static final Logger LOGGER = Logger.getLogger(UIConfig.class.getName());
     /**
      * Default number of rounds in a game.
      */
     public static final int FONT_SIZE = 24;
 
     public static void setUIFont (javax.swing.plaf.FontUIResource f){
-        java.util.Enumeration keys = UIManager.getDefaults().keys();
+        Enumeration keys = UIManager.getDefaults().keys();
         while (keys.hasMoreElements()) {
             Object key = keys.nextElement();
             Object value = UIManager.get (key);
@@ -34,16 +37,22 @@ public class UIConfig {
         }
     }
 
+    // New parameterless method
     public static void setUIFont() {
+        setUIFont(new org.springframework.core.io.DefaultResourceLoader());
+    }
+
+    // Refactored method to accept ResourceLoader
+    public static void setUIFont(ResourceLoader resourceLoader) {
         try{
-           ResourceLoader resourceLoader = new org.springframework.core.io.DefaultResourceLoader();
+           // Use the provided resourceLoader instance
            Resource resource = resourceLoader.getResource("classpath:ch/primeo/fridgely/fonts/bangers_regular.ttf");
            InputStream fontStream = resource.getInputStream();
             //File fontFile = new File("src/main/resources/ch/primeo/fridgely/fonts/bangers_regular.ttf");
             Font font = Font.createFont(Font.TRUETYPE_FONT, fontStream).deriveFont(Font.PLAIN, FONT_SIZE);
             setUIFont(new javax.swing.plaf.FontUIResource(font));
         } catch (Exception e) {
-            e.printStackTrace();
+            LOGGER.warning(e.getMessage());
             //setUIFont(new javax.swing.plaf.FontUIResource("SansSerif", Font.PLAIN, FONT_SIZE));
         }
     }
