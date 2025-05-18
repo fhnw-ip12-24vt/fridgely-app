139a0b9041fd88310852c22b2d83f93390fa8584
Diego Varela <diego.varelapenas@students.fhnw.ch>
2025-05-19 00:14:37 +0200

    Refactor UIConfig to remove static methods and improve dependency injection with ResourceLoader

--- src/main/java/ch/primeo/fridgely/config/UIConfig.java
+++ src/main/java/ch/primeo/fridgely/config/UIConfig.java
@@ -1,21 +1,20 @@
 package ch.primeo.fridgely.config;
 
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.ResourceLoader;
+import org.springframework.context.annotation.*;
+import org.springframework.core.io.*;
+import org.springframework.stereotype.*;
 
 import javax.swing.UIManager;
+import javax.swing.plaf.*;
 import java.awt.Color;
 import java.awt.Font;
 import java.io.InputStream;
 import java.util.Enumeration;
 import java.util.logging.Logger;
 
+@Component
+@Scope("singleton")
 public class UIConfig {
-
-    protected UIConfig() {
-        // Prevent instantiation
-        throw new UnsupportedOperationException("GameConfig is a utility class and cannot be instantiated");
-    }
 
     /**
      * Background color for the game UI.
@@ -38,34 +37,37 @@ public class UIConfig {
      */
     public static final int FONT_SIZE = 24;
 
-    public static void setUIFont(javax.swing.plaf.FontUIResource f) {
-        Enumeration keys = UIManager.getDefaults().keys();
+    private final ResourceLoader resourceLoader;
+
+    /**
+     * Constructor for UIConfig.
+     *
+     * @param resourceLoader the resource loader to use for loading resources
+     */
+    public UIConfig(ResourceLoader resourceLoader) {
+        this.resourceLoader = resourceLoader;
+    }
+
+    public void setUIFont(FontUIResource f) {
+        Enumeration<Object> keys = UIManager.getDefaults().keys();
         while (keys.hasMoreElements()) {
             Object key = keys.nextElement();
             Object value = UIManager.get(key);
-            if (value instanceof javax.swing.plaf.FontUIResource) {
+            if (value instanceof FontUIResource) {
                 UIManager.put(key, f);
             }
         }
     }
 
-    // New parameterless method
-    public static void setUIFont() {
-        setUIFont(new org.springframework.core.io.DefaultResourceLoader());
-    }
-
     // Refactored method to accept ResourceLoader
-    public static void setUIFont(ResourceLoader resourceLoader) {
+    public void setUIFont() {
         try {
-            // Use the provided resourceLoader instance
             Resource resource = resourceLoader.getResource("classpath:ch/primeo/fridgely/fonts/bangers_regular.ttf");
             InputStream fontStream = resource.getInputStream();
-            //File fontFile = new File("src/main/resources/ch/primeo/fridgely/fonts/bangers_regular.ttf");
             Font font = Font.createFont(Font.TRUETYPE_FONT, fontStream).deriveFont(Font.PLAIN, FONT_SIZE);
-            setUIFont(new javax.swing.plaf.FontUIResource(font));
+            setUIFont(new FontUIResource(font));
         } catch (Exception e) {
             LOGGER.warning(e.getMessage());
-            //setUIFont(new javax.swing.plaf.FontUIResource("SansSerif", Font.PLAIN, FONT_SIZE));
         }
     }
 }
