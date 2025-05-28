f7e07ae3a434fc74f0cf9598a961668f6c250322
Diego Varela <diego.varelapenas@students.fhnw.ch>
2025-05-28 18:34:07 +0200

    Refactor buildProductCard method in ProductCardUtils to enhance image handling and layout configuration

--- src/main/java/ch/primeo/fridgely/view/util/ProductCardUtils.java
+++ src/main/java/ch/primeo/fridgely/view/util/ProductCardUtils.java
@@ -1,4 +1,28 @@
 package ch.primeo.fridgely.view.util;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class ProductCardUtils {
+    public static void buildProductCard(JPanel card, JLabel nameLabel, JPanel text, ImageIcon icon, ImageIcon fallbackIcon, int imageSize){
+        nameLabel.setHorizontalAlignment(SwingConstants.CENTER);
+
+        if(text != null){
+            text.add(nameLabel, BorderLayout.NORTH);
+        }
+
+        // Product image in the bottom
+        JLabel imageLabel = new JLabel();
+
+        if (icon != null) {
+            imageLabel.setIcon(icon);
+        } else {
+            imageLabel.setIcon(fallbackIcon);
+        }
+
+        imageLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        imageLabel.setPreferredSize(new Dimension(imageSize, imageSize));
+        card.add(imageLabel, BorderLayout.CENTER);
+
+    }
 }
